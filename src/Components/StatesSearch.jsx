a7af8d3833ce9ee1469d4d3ff7e2bb3d856dbe37
Navya07122001 <navyasreeparvatham@gmail.com>
2024-08-30 18:29:51 +0530

    option values added

--- src/Components/StatesSearch.jsx
+++ src/Components/StatesSearch.jsx
@@ -46,29 +46,29 @@ const StatesSearch = () => {
         <h1>Select Location</h1>
     <div style={{margin:'2rem'}}>
   <select  style={{width:'20rem',height:'2rem'}} onChange={handleChange}>
-    <option value="volvo">Select a Country</option>
+    <option value="">Select a Country</option>
     {countrydata?.map((ele,index)=>{
         return (
-        <option key={index}>{ele}</option>
+        <option key={index} value={ele}>{ele}</option>
         )
     })
     }
   </select>
   <select  style={{width:'13rem',height:'2rem'}} onChange={handleChange1} disabled={selectedcountry?false:true}>
-    <option value="volvo">Select a State</option>
+    <option value="">Select a State</option>
     {statedata?.map((ele,index)=>{
         return (
-        <option key={index} >{ele}</option>
+        <option key={index} value={ele} >{ele}</option>
         )
     })
     }
   </select>
 
   <select  style={{width:'13rem',height:'2rem'}} onChange={handleChange2} disabled={selectedstate?false:true}>
-    <option value="volvo">Select a City</option>
+    <option value="">Select a City</option>
     {citydata?.map((ele,index)=>{
         return (
-        <option key={index} >{ele}</option>
+        <option key={index} value={ele} >{ele}</option>
         )
     })
     }
