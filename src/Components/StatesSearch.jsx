6d4a8dfcc1cc1a5c9c6859c891e8c62f1c7a0f3d
Navya07122001 <navyasreeparvatham@gmail.com>
2024-08-30 18:43:57 +0530

    font size changed

--- src/Components/StatesSearch.jsx
+++ src/Components/StatesSearch.jsx
@@ -1,86 +1,90 @@
-import React, { useEffect,useState } from 'react'
-import {fetchCountries,fetchStates,fetchCities} from '../data';
+import React, { useEffect, useState } from 'react'
+import { fetchCountries, fetchStates, fetchCities } from '../data';
 const StatesSearch = () => {
-    const [countrydata,setCountrydata]=useState([]);
-    const [selectedcountry,setSelectedcountry]=useState();
-    const [statedata,setStatedata]=useState([]);
-    const [citydata,setCitydata]=useState([]);
-    const [selectedstate,setSelectedstate]=useState('');
-    const [selectedcity,setSelectedcity]=useState('');
-  useEffect(()=>{
-    const getdata=async()=>{
-    const data=await fetchCountries();
-    setCountrydata(data);
-    }
-   getdata();
-  },[])
+    const [countrydata, setCountrydata] = useState([]);
+    const [selectedcountry, setSelectedcountry] = useState();
+    const [statedata, setStatedata] = useState([]);
+    const [citydata, setCitydata] = useState([]);
+    const [selectedstate, setSelectedstate] = useState('');
+    const [selectedcity, setSelectedcity] = useState('');
+    useEffect(() => {
+        const getdata = async () => {
+            const data = await fetchCountries();
+            setCountrydata(data);
+        }
+        getdata();
+    }, [])
 
-  const handleChange=(e)=>{
-    console.log(e.target.value)
-    setSelectedcountry(e.target.value)
-  }
-  const handleChange2=(e)=>{
-    console.log(e.target.value)
-    setSelectedcity(e.target.value)
-  }
-  useEffect(()=>{
-    const getdata=async()=>{
-     const data=await fetchStates(selectedcountry);
-     setStatedata(data);
+    const handleChange = (e) => {
+        console.log(e.target.value)
+        setSelectedcountry(e.target.value)
     }
-    getdata()
-  },[selectedcountry])
-   const handleChange1=(e)=>{
-    console.log(e.target.value)
-    setSelectedstate(e.target.value)
-   }
-   useEffect(()=>{
-    const getdata=async()=>{
-        const data=await fetchCities(selectedcountry,selectedstate);
-        setCitydata(data);
-       }
-       getdata()
-   },[selectedcountry,selectedstate])
-  return (
-    <div>
-        <h1>Select Location</h1>
-    <div style={{margin:'2rem'}}>
-  <select  style={{width:'20rem',height:'2rem'}} onChange={handleChange}>
-    <option value="">Select a Country</option>
-    {countrydata?.map((ele,index)=>{
-        return (
-        <option key={index} value={ele}>{ele}</option>
-        )
-    })
+    const handleChange2 = (e) => {
+        console.log(e.target.value)
+        setSelectedcity(e.target.value)
     }
-  </select>
-  <select  style={{width:'13rem',height:'2rem'}} onChange={handleChange1} disabled={selectedcountry?false:true}>
-    <option value="">Select a State</option>
-    {statedata?.map((ele,index)=>{
-        return (
-        <option key={index} value={ele} >{ele}</option>
-        )
-    })
+    useEffect(() => {
+        const getdata = async () => {
+            const data = await fetchStates(selectedcountry);
+            setStatedata(data);
+        }
+        getdata()
+    }, [selectedcountry])
+    const handleChange1 = (e) => {
+        console.log(e.target.value)
+        setSelectedstate(e.target.value)
     }
-  </select>
+    useEffect(() => {
+        const getdata = async () => {
+            const data = await fetchCities(selectedcountry, selectedstate);
+            setCitydata(data);
+        }
+        getdata()
+    }, [selectedcountry, selectedstate])
+    return (
+        <div>
+            <h1>Select Location</h1>
+            <div style={{ margin: '2rem' }}>
+                <select style={{ width: '20rem', height: '2rem' }} onChange={handleChange}>
+                    <option value="">Select a Country</option>
+                    {countrydata?.map((ele, index) => {
+                        return (
+                            <option key={index} value={ele}>{ele}</option>
+                        )
+                    })
+                    }
+                </select>
+                <select style={{ width: '13rem', height: '2rem' }} onChange={handleChange1} disabled={selectedcountry ? false : true}>
+                    <option value="">Select a State</option>
+                    {statedata?.map((ele, index) => {
+                        return (
+                            <option key={index} value={ele} >{ele}</option>
+                        )
+                    })
+                    }
+                </select>
 
-  <select  style={{width:'13rem',height:'2rem'}} onChange={handleChange2} disabled={selectedstate.length?false:true}>
-    <option value="">Select a City</option>
-    {citydata?.map((ele,index)=>{
-        return (
-        <option key={index} value={ele} >{ele}</option>
-        )
-    })
-    }
-  </select>
-  { selectedcity &&
-<div>
-    You Selected <span style={{fontSize:'1.5rem',fontWeight:'500'}}>{selectedcountry}</span>,{selectedstate},{selectedcity}
-</div>
-}
-    </div>
-    </div>
-  )
+                <select style={{ width: '13rem', height: '2rem' }} onChange={handleChange2} disabled={selectedstate.length ? false : true}>
+                    <option value="">Select a City</option>
+                    {citydata?.map((ele, index) => {
+                        return (
+                            <option key={index} value={ele} >{ele}</option>
+                        )
+                    })
+                    }
+                </select>
+                {selectedcity &&
+                    <div>
+                        <span>
+                            <span style={{fontSize:"1.3rem",fontWeight:'400'}}>You Selected</span>
+                            <span style={{ fontSize: '1.5rem', fontWeight: '500' }}> {selectedcountry} </span>, 
+                            {selectedstate},{selectedcity}
+                        </span>
+                    </div>
+                }
+            </div>
+        </div>
+    )
 }
 
 export default StatesSearch;
